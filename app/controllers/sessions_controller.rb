84f7491b0cb1dc6eb8f0c08966025aad78c3b274
JTSwisher <jefftswisher@gmail.com>
2020-04-09 12:39:59 -0700

    Add flash alert for login errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id 
             redirect_to user_path(@user)
         else 
-            render :new, flash[:alert] = "Your login credentials were incorrect. Please try again."
+            flash[:alert] = "Your login credentials were incorrect. Please try again."
+            redirect_to new_session_path
         end 
     end
 
