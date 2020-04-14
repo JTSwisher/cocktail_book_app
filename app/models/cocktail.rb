a6e7c99f6321f7bd295875351a68c8bfb4938b5d
JTSwisher <jefftswisher@gmail.com>
2020-04-14 10:10:07 -0700

    Add reject_if method to Cocktail model, to reject empty hash values on nested attributes

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,7 +3,11 @@ class Cocktail < ActiveRecord::Base
     has_many :cocktail_ingredients
     has_many :ingredients, through: :cocktail_ingredients
 
-    accepts_nested_attributes_for :cocktail_ingredients
+    validates :name, presence: true
+    validates :instructions, presence: true 
+
+    accepts_nested_attributes_for :cocktail_ingredients, :reject_if => proc { |attrs| attrs[:quantity].blank? && attrs[:ingredient_attributes][:name].blank?}
+       
     
 
 end 
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,5 @@ class Ingredient < ActiveRecord::Base
     has_many :cocktail_ingredients
     has_many :cocktails, through: :cocktail_ingredients    
 
+    validates :name, presence: true
 end 
