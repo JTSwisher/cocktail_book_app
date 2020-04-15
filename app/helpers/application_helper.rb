187c57a2ba59fc05e86323ab7f509f6f3681e7d5
JTSwisher <jefftswisher@gmail.com>
2020-04-15 11:20:29 -0700

    Add current_path helper to adjust view displays based on user nested paths

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    def current_path
+        current_uri = request.env['PATH_INFO']
+        if current_uri.include?(current_user.id.to_s)
+            return true
+        else 
+            return false
+        end 
+    end 
+
 end
