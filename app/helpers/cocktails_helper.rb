a8f21901932e84c450166525599ccdbb2c3318fd
JTSwisher <jefftswisher@gmail.com>
2020-04-15 13:21:49 -0700

    Add cocktail_owner helper

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,11 @@
 module CocktailsHelper
+
+    def cocktail_owner(c)
+        if c.user_id == current_user.id
+            content_tag :td, "Me"
+        else
+            content_tag :td, "#{c.user.name}"
+        end
+    end
+
 end
